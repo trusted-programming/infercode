e118ed93942c995706a242d9a544b43b24139808
Yijun Yu <y.yu@open.ac.uk>
2025-08-09 08:26:53 +0000

    dependencies

--- i.sh
+++ i.sh
@@ -1,9 +1,18 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.9 python3.9-venv python3.9-dev
+python3.9 -m venv 3.9
+source 3.9/bin/activate
+pip install --upgrade pip
 pip install sentencepiece
 pip install dpu_utils
-pip install tree_sitter
+pip install tree_sitter==0.19.0
 pip install bidict
-pip install tensorflow
+pip install tensorflow==2.5.0
 for f in tree-sitter-asm-0.0.1 tree-sitter-c-0.19.0 tree-sitter-cpp-0.19.0 tree-sitter-c-sharp-0.19.0 tree-sitter-kotlin-0.2.6 tree-sitter-rust-0.19.1; do
 	rm -rf ~/.tree-sitter/bin/$f
 done
+pip install tree-sitter-parsers
 python test.py
+cd ~/.tree-sitter
+unzip Linux.zip
+cd -
