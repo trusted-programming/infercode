f4093652d9211fe09f59f6acf82f5aa6104cbf5d
Yijun Yu <y.yu@open.ac.uk>
2025-08-11 21:52:58 +0000

    update

--- i.sh
+++ i.sh
@@ -1,8 +1,8 @@
-# sudo add-apt-repository ppa:deadsnakes/ppa
-# sudo apt install python3.9 python3.9-venv python3.9-dev
-# python3.9 -m venv 3.9
-# source 3.9/bin/activate
-# pip install --upgrade pip
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt install python3.9 python3.9-venv python3.9-dev -y
+python3.9 -m venv 3.9
+source 3.9/bin/activate
+pip install --upgrade pip
 pip install infercode
 pip install tensorflow==2.5.0
 pip install typing-extensions --upgrade
